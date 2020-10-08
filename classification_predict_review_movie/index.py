ac69f23d761dbfdd51f0eb1bcd3a17215b60fb6f
Thai Loc <kunkaamd@gmail.com>
2020-10-08 10:05:31 +0700

    update binary classification

--- classification_predict_review_movie/index.py
+++ classification_predict_review_movie/index.py
@@ -21,11 +21,99 @@ dataset_dir = './aclImdb'
 
 train_dir = os.path.join(dataset_dir, 'train')
 
-sample_file = os.path.join(train_dir, 'pos/1181_9.txt')
-with open(sample_file) as f:
-  print(f.read())
+
+# remove data unused
+# remove_dir = os.path.join(train_dir, 'unsup')
+# shutil.rmtree(remove_dir)
+
+# Load data 
+batch_size = 32 
+seed = 42 # random seed for shuffling and transformations. (một số random)
+# Một Epoch được tính là khi chúng ta đưa tất cả dữ liệu vào mạng neural network 1 lần.
+# batch size 1 phần dữ liêu trong 1 epoch vd: dataset = 320,batch_size = 32, 1 batch = 10,
+# Iterations là số lượng batchs cần để hoàn thành 1 epoch.
+
+raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train', 
+    batch_size=batch_size, 
+    validation_split=0.2,#train=80%,validation=20%,
+    subset='training',# 'training' or 'validation'
+    seed=seed)
 
 
-# LOAD DATASET
-remove_dir = os.path.join(train_dir, 'unsup')
-shutil.rmtree(remove_dir)
+raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train', 
+    batch_size=batch_size, 
+    validation_split=0.2, 
+    subset='validation', 
+    seed=seed)
+
+raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/test', 
+    batch_size=batch_size)
+
+
+# remove the html (standardize)
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+  return tf.strings.regex_replace(stripped_html,
+                                  '[%s]' % re.escape(string.punctuation),
+                                  '')
+
+
+
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = TextVectorization(
+    standardize=custom_standardization,
+# max_tokens: The maximum size of the vocabulary for this layer.
+# If None, there is no cap on the size of the vocabulary.
+# Note that this vocabulary contains 1 OOV token, so the effective number of tokens is (max_tokens - 1 - (1 if output == "int" else 0)).
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
+
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+def vectorize_text(text, label):
+  text = tf.expand_dims(text, -1)
+  return vectorize_layer(text), label
+
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
+test_ds = raw_test_ds.map(vectorize_text)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+# CREATE MODEL
+embedding_dim = 16
+model = tf.keras.Sequential([
+  layers.Embedding(max_features + 1, embedding_dim),
+  layers.Dropout(0.2),
+  layers.GlobalAveragePooling1D(),
+  layers.Dropout(0.2),
+  layers.Dense(1)])
+
+model.summary()
+
+model.compile(loss=losses.BinaryCrossentropy(from_logits=True), optimizer='adam', metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
+
+epochs = 10
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs)
+
+
+loss, accuracy = model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
